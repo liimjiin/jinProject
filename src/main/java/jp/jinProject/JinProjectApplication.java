1856abc9443beae944a3ccabc49f870c6e5e9e80
liimjiin <limiary27@gmail.com>
2024-04-11 17:28:34 +0900

    Initial commit

---
+++ src/main/java/jp/jinProject/JinProjectApplication.java
@@ -0,0 +1,13 @@
+package jp.jinProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/jinProject/JinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.jinProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
